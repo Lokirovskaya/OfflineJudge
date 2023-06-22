5e301e05823fecaddce9aa6df5a6cd225e02a133
Lokirovskii <719220502@qq.com>
2023-06-22 13:16:35 +0800

    wa的文件展示

--- judge.py
+++ judge.py
@@ -1,9 +1,8 @@
 ########### Config Here ###########
 
-TESTCASE_PATH = 'testcase/'
 SRC_DIR_PATH = 'aa.c'
 COMPILE_TIMEOUT = 5
-EXEC_TIMEOUT = 20
+EXEC_TIMEOUT = 60
 
 ###################################
 
@@ -40,7 +39,7 @@ def compile_compiler():
     pass
 
 
-def source_to_llvm(source_path, llvm_path) -> (bool, Optional[str]):
+def source_to_llvm(source_path, llvm_path, **kwargs) -> (bool, Optional[str]):
     args = [
         'clang',
         '-S',
@@ -52,14 +51,16 @@ def source_to_llvm(source_path, llvm_path) -> (bool, Optional[str]):
         '-w'
     ]
     try:
-        r = sp.run(args, timeout=COMPILE_TIMEOUT, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+        r = sp.run(args, timeout=COMPILE_TIMEOUT, stdout=sp.DEVNULL, stderr=sp.PIPE)
     except sp.TimeoutExpired:
         return (
             False,
-            f'{YELLOW}{BOLD}Time Limit Exceeded{END}{YELLOW}: Source -> LLVM IR ({COMPILE_TIMEOUT} s){END}'
+            f'{YELLOW}{BOLD}Time Limit Exceeded{END}{YELLOW}: Running Source -> LLVM IR ({COMPILE_TIMEOUT} s){END}'
         )
 
-    if r.returncode != 0: 
+    if r.returncode != 0:
+        with open(kwargs[re_path], 'w') as re_file:
+            re_file.write(r.stderr.decode('utf-8'))
         return (
             False,
             f'{YELLOW}{BOLD}Runtime Error{END}{YELLOW}: Source -> LLVM IR{END}'
@@ -67,7 +68,7 @@ def source_to_llvm(source_path, llvm_path) -> (bool, Optional[str]):
     return (True, None)
 
 
-def llvm_to_exe(llvm_path, exe_path) -> (bool, Optional[str]):
+def llvm_to_exe(llvm_path, exe_path, **kwargs) -> (bool, Optional[str]):
     args = [
         'clang',
         llvm_path,
@@ -76,8 +77,10 @@ def llvm_to_exe(llvm_path, exe_path) -> (bool, Optional[str]):
         exe_path,
         '-O0'
     ]
-    r = sp.run(args, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+    r = sp.run(args, stdout=sp.DEVNULL, stderr=sp.PIPE)
     if r.returncode != 0:
+        with open(kwargs[re_path], 'w') as re_file:
+            re_file.write(r.stderr.decode('utf-8'))
         return (
             False,
             f'{YELLOW}{BOLD}Runtime Error{END}{YELLOW}: LLVM IR -> ASM{END}'
@@ -85,19 +88,19 @@ def llvm_to_exe(llvm_path, exe_path) -> (bool, Optional[str]):
     return (True, None)
 
 
-def run_exe(exe_path, input_path, answer_path) -> (bool, Optional[str], Optional[int], Optional[str]):
+# return: status, stdout, returncode, tle_msg 
+def run_exe(exe_path, input_path, **kwargs) -> (bool, Optional[str], Optional[int], Optional[str]):
     args = ['./' + exe_path]
     try:
-        with open(answer_path, 'w') as answer_file:
-            # stdin available
-            if os.path.exists(input_path):
-                with open(input_path, 'r') as input_file:
-                    r = sp.run(args, stdin=input_file, stdout=sp.PIPE, stderr=sp.DEVNULL, timeout=EXEC_TIMEOUT)
-                    return (True, r.stdout, r.returncode, None)
-            # no stdin
-            else:
-                r = sp.run(args, stdout=sp.PIPE, stderr=sp.DEVNULL, timeout=EXEC_TIMEOUT)
-                return (True, r.stdout, r.returncode, None)
+        # stdin available
+        if os.path.exists(input_path):
+            with open(input_path, 'r') as input_file:
+                r = sp.run(args, stdin=input_file, stdout=sp.PIPE, stderr=sp.DEVNULL, timeout=EXEC_TIMEOUT)
+                return (True, r.stdout.decode('utf-8'), r.returncode, None)
+        # no stdin
+        else:
+            r = sp.run(args, stdout=sp.PIPE, stderr=sp.DEVNULL, timeout=EXEC_TIMEOUT)
+            return (True, r.stdout.decode('utf-8'), r.returncode, None)
     except sp.TimeoutExpired:
         return (
             False,
@@ -107,26 +110,39 @@ def run_exe(exe_path, input_path, answer_path) -> (bool, Optional[str], Optional
         )
 
 
-def check_answer(answer_str, correct_path, return_code, wa_path) -> (bool, str):
+def check_answer(answer_str, correct_path, return_code, **kwargs) -> (bool, str):
+    def dump_wa(wa_lines):
+        with open(kwargs[wa_path], 'w') as wa_file:
+            wa_file.writelines(wa_lines)
+    def create_diff_bat():
+        with open(kwargs[diff_path], 'w') as diff_file:
+            diff_file.write(f'code --diff ../{kwargs[wa_path]} ../{correct_path}')
+            
     with open(correct_path, 'r') as correct_file:
-        ans_lines = answer_str.rstrip().splitlines()
+        # strip line ends and file end
+        ans_lines = [line.rstrip() for line in answer_str.rstrip().splitlines()] 
         ans_lines.append(str(return_code))
 
-        correct = correct_file.read()
-        correct_lines = correct.rstrip().splitlines()
+        correct_str = correct_file.read()
+        correct_lines = [line.rstrip() for line in correct_str.rstrip().splitlines()] 
 
-        min_len = min(len(ans_lines), len(correct_lines))
-        for i in range(min_len):
-            if ans_lines[i].rstrip() != correct_lines[i].rstrip():
-                return (
-                    False,
-                    f'{RED}{BOLD}Wrong Answer{END}{RED}: Line {i + 1}{END}'
-                )
-        if len(ans_lines) != min_len:
+        if len(ans_lines) != len(correct_lines):
+            dump_wa(ans_lines)
+            create_diff_bat()
             return (
                 False,
-                f'{RED}{BOLD}Wrong Answer{END}{RED}: Line {min_len + 1}{END}'
+                f'{RED}{BOLD}Wrong Answer{END}'
             )
+            
+        for i in range(len(ans_lines)):
+            if ans_lines[i] != correct_lines[i]:
+                dump_wa(ans_lines)
+                create_diff_bat()
+                return (
+                    False,
+                    f'{RED}{BOLD}Wrong Answer{END}'
+                )
+                
         return (True, f'{GREEN}{BOLD}Accepted{END}')
 
 
@@ -137,21 +153,22 @@ def judge_one(test_name, idx, total) -> bool:
     
     llvm_path = f'tmp/{idx}.ll'
     exe_path = f'tmp/{idx}.exe'
-    answer_path = f'tmp/{idx}.out'
 
-    wa_path = f'wa/{idx}_{test_name.replace("/", "_")}.out'
+    wa_path = f'wa/wa_out/{idx}_WA_{test_name.replace("/", "_")}.out'
+    diff_path = f'wa/{idx}_WA_show_diff.bat'
+    re_path = f'wa/{idx}_RE_{test_name.replace("/", "_")}.txt'
 
     def run() -> (bool, str):
-        ok, msg = source_to_llvm(source_path, llvm_path)
+        ok, msg = source_to_llvm(source_path, llvm_path, re_path=re_path)
         if not ok: return ok, msg
 
-        ok, msg = llvm_to_exe(llvm_path, exe_path)
+        ok, msg = llvm_to_exe(llvm_path, exe_path, re_path=re_path)
         if not ok: return ok, msg
 
-        ok, answer, return_code, msg = run_exe(exe_path, input_path, answer_path)
+        ok, answer, return_code, msg = run_exe(exe_path, input_path)
         if not ok: return ok, msg
 
-        ok, msg = check_answer(answer, correct_path, return_code, wa_path)
+        ok, msg = check_answer(answer, correct_path, return_code, wa_path=wa_path, diff_path=diff_path)
         return ok, msg
 
     ok, msg = run()
@@ -165,13 +182,18 @@ def cleanup():
 
 
 def init():
-    if not os.path.exists('out/'):
-        os.makedirs('out/')
-    if not os.path.exists('wa/'):
-        os.makedirs('wa/')
-    if os.path.exists('tmp/'):
-        shutil.rmtree('tmp/')
-    os.makedirs('tmp/')
+    def mkdir(dir_path):
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+    def mkdir_empty(dir_path):
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+        os.makedirs(dir_path)
+        
+    mkdir('out')
+    mkdir_empty('tmp')
+    mkdir_empty('wa')
+    mkdir('wa/wa_out')
 
     def signal_handler(signal, frame):
         total = passed + failed
@@ -205,7 +227,7 @@ if __name__ == '__main__':
 
     result_list = [result.get() for result in result_list]
     passed = result_list.count(True)
-    total = len(test_list)
+    total = len(result_list)
               
     print(f'\n{BOLD}Test Finished!{END} {GREEN}{BOLD}Accepted: ({passed}/{total}){END} {RED}{BOLD}Failed: ({total - passed}/{total}){END}')
     cleanup()
